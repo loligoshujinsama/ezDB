df3a32079fd0a40be81b43546a69a30f029b0c7f
loligoshujinsama <57374468+loligoshujinsama@users.noreply.github.com>
2023-10-28 22:49:13 +0800

    Update showall_db.c

--- showall_db.c
+++ showall_db.c
@@ -1,32 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+int choice(const char *command) {
+	if (strcmp(command,"OPEN") == 0 || strcmp(command,"open") == 0 ) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
 
 int main()
 {
 	FILE* ptr;
 	char ch;
+	char command[255];
+	char file[255];
+	printf("Welcome to ezDB, input your command below\n> ");
+	scanf("%s %s",command,file);
+	file[strcspn(file, "\n")] = '\0';
+	command[strcspn(command, "\n")] = '\0';
 
-	// Edit your directory here for now 
-	ptr = fopen("C:\\Users\\joelc\\Downloads\\C Programming Files\\FruitPrice.txt", "r");
-
-	if (NULL == ptr) {
-		printf("file can't be opened \n");
+	int result = choice(command);
+	if (result == 1) {
+		printf("%s\n",file);
+		ptr = fopen(file, "r");
+		// If file exists
+		if (ptr != NULL) {
+			do {
+				ch = fgetc(ptr);
+				printf("%c", ch);
+			} while (ch != EOF);
+			fclose(ptr);
+		} else {
+			printf("Error opening file \n");
+		}
 	}
 
-	printf("There are in total x records found \n");
 
-	// Printing what is written in file
-	// character by character using loop.
-	do {
-		ch = fgetc(ptr);
-		printf("%c", ch);
-
-		// Checking if character is not EOF.
-		// If it is EOF stop reading.
-	} while (ch != EOF);
-
-	// Closing the file
-	fclose(ptr);
-	return 0;
 }
